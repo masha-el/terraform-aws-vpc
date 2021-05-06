209c9083f02b7218a15ae5853ce61a844c30c81a
Maria Elgart <masha.elgart@gmail.com>
2021-05-06 09:18:04 +0300

    Add files via upload
    add outputs file

---
+++ outputs.tf
@@ -0,0 +1,54 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = concat(aws_vpc.this.*.id, [""])[0]
+}
+
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = concat(aws_vpc.this.*.cidr_block, [""])[0]
+}
+
+output "vpc_instance_tenancy" {
+  description = "Tenancy of instances spin up within VPC"
+  value       = concat(aws_vpc.this.*.instance_tenancy, [""])[0]
+}
+
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = aws_subnet.private.*.id
+}
+
+output "private_subnets_cidr_blocks" {
+  description = "List of cidr_blocks of private subnets"
+  value       = aws_subnet.private.*.cidr_block
+}
+
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value       = aws_subnet.public.*.id
+}
+
+output "public_subnets_cidr_blocks" {
+  description = "List of cidr_blocks of public subnets"
+  value       = aws_subnet.public.*.cidr_block
+}
+
+output "natgw_ids" {
+  description = "List of NAT Gateway IDs"
+  value       = aws_nat_gateway.this.*.id
+}
+
+output "igw_id" {
+  description = "The ID of the Internet Gateway"
+  value       = concat(aws_internet_gateway.this.*.id, [""])[0]
+}
+
+output "azs" {
+  description = "A list of availability zones specified as argument to this module"
+  value       = var.azs
+}
+
+output "name" {
+  description = "The name of the VPC specified as argument to this module"
+  value       = var.name
+}
